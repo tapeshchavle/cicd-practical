0b5792fbb8fdfe4e3627625088c2ae4d4f582502
Tapesh Chavle <tapeshchawle@gmail.com>
2025-09-13 00:01:30 +0530

    add files

---
+++ src/main/java/demo_project/demo/GithubCicdApplication.java
@@ -0,0 +1,13 @@
+package demo_project.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdApplication.class, args);
+	}
+
+}
---
+++ src/main/java/demo_project/demo/HelloController.java
@@ -0,0 +1,12 @@
+package demo_project.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String home() {
+        return "Hello from Spring Boot 🚀";
+    }
+}
